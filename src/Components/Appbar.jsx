4dd1cdf03000406ea586fe92af1abfb42b4e3820
M.Hassan Amir <hassanamir0506@gmail.com>
2024-07-26 13:09:03 +0500

    added padding in the appbar

--- src/Components/Appbar.jsx
+++ src/Components/Appbar.jsx
@@ -4,7 +4,7 @@ const Appbar = (props) => {
     return (
         <div className="bg-sideBarColor h-[70px] pt-5 flex justify-between items-center pr-6 py-10 px-10  sticky " style={{ fontFamily: 'Poppins, arial' }}>
 
-            <div className="logo top-4 text-center text-3xl text-white">
+            <div className="logo top-4 text-center text-3xl text-white mt-4">
                 <img src="/src/assets/eventinoLogo.png" alt="Eventino Logo" className="h-auto w-[130px] block mx-auto mt-2" />
             </div>
 
